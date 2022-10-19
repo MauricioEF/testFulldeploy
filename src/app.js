0d03d9944911876e05e436f927e1ddec940cb3b3
Mauricio Espinosa <ing_mauricioespinosa@hotmail.com>
2022-10-18 21:43:41 -0500

    endpoint

--- src/app.js
+++ src/app.js
@@ -3,3 +3,7 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT||8080;
 app.listen(PORT,()=>console.log("Listening"))
+
+app.get('/',(req,res)=>{
+    console.log("Listening");
+})
